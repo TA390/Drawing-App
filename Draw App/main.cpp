d1b169ffee241a54ad447375c8e7bcab7714f75d
Taha Abdalla <tahaabdalla@Tahas-MacBook-Pro.local>
2017-05-20 12:12:52 +0100

    Initial Commit

---
+++ Draw App/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Draw App
+//
+//  Created by Taha Abdalla on 20/05/2017.
+//  Copyright © 2017 TA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
