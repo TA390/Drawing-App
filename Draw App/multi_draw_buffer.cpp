4bf578713d48c6dcbb9ff0dea06ee3e4fdd51fb9
Taha Abdalla <tahaabdalla@Tahas-MBP.home>
2017-05-26 12:44:38 +0100

    Multi draw buffer v1.0 untested

--- Draw App/multi_draw_buffer.cpp
+++ Draw App/multi_draw_buffer.cpp
@@ -6,6 +6,81 @@
 //  Copyright © 2017 TA. All rights reserved.
 //
 
-//#include "multi_draw_buffer.h"
+#include "multi_draw_buffer.h"
+
+/* ------------------------------------------------------------------------------------ */
+
+MultiDrawBuffer::MultiDrawBuffer()
+: BaseBuffer{static_cast<size_t>(std::pow(2.0, 20.0)), 3}
+{
+    
+}
+
+/* ------------------------------------------------------------------------------------ */
+
+MultiDrawBuffer::~MultiDrawBuffer()
+{
+    
+}
+
+/* ------------------------------------------------------------------------------------ */
+
+void MultiDrawBuffer::start_segment()
+{
+    commands.push_back(DrawArraysIndirectCommand{0, 1, (unsigned)index, 0});
+}
+/* ------------------------------------------------------------------------------------ */
+
+void MultiDrawBuffer::end_segment()
+{
+    if(commands.size() != 0 && commands.back().count == 0) {
+        commands.pop_back();
+    }
+}
+
+/* ------------------------------------------------------------------------------------ */
+
+bool MultiDrawBuffer::write_to_buffer(float xpos, float ypos)
+{
+    lock.remove_signaled_locks(0);
+    glBindBuffer(GL_DRAW_INDIRECT_BUFFER, _vbo);
+    GLuint start_index = index * _stride;
+    GLfloat* data = (GLfloat*)glMapBufferRange(GL_DRAW_INDIRECT_BUFFER, start_index, _data_size, flags);
+    
+    if(data != nullptr) {
+        *data++ = xpos;
+        *data++ = ypos;
+        *data = 0.0;
+        glFlushMappedBufferRange(GL_DRAW_INDIRECT_BUFFER, start_index, _data_size);
+    }
+    
+    bool write_successful = glUnmapBuffer(GL_DRAW_INDIRECT_BUFFER);
+    glBindBuffer(GL_DRAW_INDIRECT_BUFFER, 0);
+    
+    if(write_successful) {
+        ++index;
+        commands.back().count = commands.back().count + 1;
+    }
+    
+    return write_successful;
+}
+
+/* ------------------------------------------------------------------------------------ */
+
+void MultiDrawBuffer::draw()
+{
+    glBindVertexArray(_vao);
+    glMultiDrawArraysIndirect(GL_LINE_STRIP, &*commands.begin(), commands.size(), 0);
+    lock.lock_range((index-1)*_stride, _data_size);
+    fence = glFenceSync(GL_SYNC_GPU_COMMANDS_COMPLETE, 0);
+    glBindVertexArray(0);
+}
+
+/* ------------------------------------------------------------------------------------ */
+
+void MultiDrawBuffer::resize()
+{
+
+}
 
 /* ------------------------------------------------------------------------------------ */
